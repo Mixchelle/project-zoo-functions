36883b5290500b7e06caee37a32c3294ad2686d8
Mixchelle <michelle.biologia@hotmail.com>
2022-09-29 19:04:04 -0300

    Requisito 1 Feito depois de um dia todo

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,6 +1,6 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {1
+function countEntrants(entrants) {
   // seu código aqui
 }
 
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) return [];
+  return species.filter(({ id }) => ids.includes(id));
 }
 
 module.exports = getSpeciesByIds;
