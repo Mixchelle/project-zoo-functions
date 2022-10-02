97bfbe9ede5811b9805f4c2856d34a82359a3cd4
Mixchelle <michelle.biologia@hotmail.com>
2022-10-01 21:20:43 -0300

    Requisito 8"" git push

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,24 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const obj = {
+    child: entrants.filter((visitante) => visitante.age < 18).length,
+    adult: entrants.filter((visitante) => visitante.age >= 18 && visitante.age < 50).length,
+    senior: entrants.filter((visitante) => visitante.age >= 50).length,
+  };
+  return obj;
 }
-
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants) {
+    return 0;
+  } if (Object.values(entrants).length === 0) {
+    return 0;
+  } const QuantVisit = countEntrants(entrants);
+  const childN = QuantVisit.child * prices.child;
+  const adultN = QuantVisit.adult * prices.adult;
+  const seniorN = QuantVisit.senior * prices.senior;
+  const soma = childN + adultN + seniorN;
+  return soma;
 }
 
 module.exports = { calculateEntry, countEntrants };
